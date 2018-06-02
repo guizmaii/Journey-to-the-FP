d6df4591b8daae228fc0bf4064882b4efe062752
jules Ivanic <jules.ivanic@gmail.com>
2018-06-02 22:55:32 +0200

    Clean

--- src/test/scala/com/guizmaii/utils/ScalaUtils.scala
+++ src/test/scala/com/guizmaii/utils/ScalaUtils.scala
@@ -7,10 +7,11 @@ object ScalaUtils {
   lazy val isScala211: Boolean = scalaVersion.contains("2.11")
   lazy val isScala212: Boolean = scalaVersion.contains("2.12")
 
-  lazy val globalExecutionThreadPoolName: String = (isScala211, isScala212) match {
-    case (true, _) => "ForkJoinPool-1-worker"
-    case (_, true) => "scala-execution-context-global"
-    case _         => throw new RuntimeException(s"This code is not yet compatible with Scala $scalaVersion")
-  }
+  lazy val globalExecutionThreadPoolName: String =
+    (isScala211, isScala212) match {
+      case (true, _) => "ForkJoinPool-1-worker"
+      case (_, true) => "scala-execution-context-global"
+      case _         => throw new RuntimeException(s"This code is not yet compatible with Scala $scalaVersion")
+    }
 
 }
